a9942e0fd546b088552dbbf3b01176df93bc850f
vianneyKB <vianneykondo@yahoo.fr>
2023-02-23 01:38:43 -0800

    fixing 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n <= 58; n++)
+	for (n = 0; n <= 7; n++)
 	{
-		for (m = 49; m <= 58; m++)
+		for (m = n + 1; m <= 8; m++)
 		{
-			for (l = 50; l <= 58; l++)
+			for (l = m + 1; l <= 9; l++)
 			{
 				if (l > m && m > n)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
+					putchar(n + '0');
+					putchar(m + '0');
+					putchar(l + '0');
+					if (n != 7 || m != 8 || l != 9)
 					{
 						putchar(',');
 						putchar(' ');
