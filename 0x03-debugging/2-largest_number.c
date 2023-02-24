539608e16b205b94f881281b18ac0fb8859e8e38
vianneyKB <vianneykondo@yahoo.fr>
2023-02-23 16:21:14 -0800

    fixing space erros

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,8 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+	largest = (a > b) ? a : b;
+	largest = (largest > c) ? largest : c;
 
 	return (largest);
 }
